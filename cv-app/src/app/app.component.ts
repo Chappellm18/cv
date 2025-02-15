a599b7f6a776ae556bfaf2929b9a0c6fb87c0c5d
mchappell18 <chapps360@gmail.com>
2025-02-15 17:48:36 -0500

    stuff

--- cv-app/src/app/app.component.ts
+++ cv-app/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
--- cv-app/src/app/app.routes.ts
+++ cv-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { InfoComponent } from './components/info/info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LandingComponent},
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'info', component: InfoComponent}
+];
