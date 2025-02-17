1e3f45902c4b3b7b782b96fdb968235561ce30e5
mchappell18 <chapps360@gmail.com>
2025-02-17 07:18:24 -0500

    more updates

--- cv-app/src/app/components/landing/landing.component.ts
+++ cv-app/src/app/components/landing/landing.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
+import { RouterModule } from '@angular/router'
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [MatCardModule, MatIconModule,MatButtonModule],
+  imports: [
+    MatCardModule, 
+    MatIconModule,
+    MatButtonModule, 
+    RouterModule
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
--- cv-app/src/app/components/projects/projects.component.ts
+++ cv-app/src/app/components/projects/projects.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Projects {
+  name: string;
+  imageUrl: string;
+  description: string;
+  githubLink: string;
+  lastUpdated: Date; // Find a github API and link the repo and the data
+
+}
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  projects: Projects[] = [
+    {name: 'Test', imageUrl: '', description: 'Testing', githubLink: '', lastUpdated: new Date()}
+  ]
 }
