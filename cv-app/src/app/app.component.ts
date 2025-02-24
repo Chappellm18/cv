3e6c51989d19d2fbc4d701e5b0462d5aa8cc9537
mchappell18 <chapps360@gmail.com>
2025-02-24 05:18:33 -0500

    have a lot more style work to do

--- cv-app/src/app/app.component.ts
+++ cv-app/src/app/app.component.ts
@@ -4,12 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
+import { LandingComponent } from './components/landing/landing.component';
+import { InfoComponent } from './components/info/info.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LandingComponent, InfoComponent, ProjectsComponent, TimelineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- cv-app/src/app/components/projects/projects.component.ts
+++ cv-app/src/app/components/projects/projects.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 interface Projects {
-  name: string;
-  imageUrl: string;
+  title: string;
+  company: string;
+  technologies: string[];
   description: string;
+  imageUrl: string;
   githubLink: string;
-  lastUpdated: Date; // Find a github API and link the repo and the data
-
 }
 
 @Component({
@@ -19,6 +19,37 @@ interface Projects {
 })
 export class ProjectsComponent {
   projects: Projects[] = [
-    {name: 'Test', imageUrl: '', description: 'Testing', githubLink: '', lastUpdated: new Date()}
-  ]
+    {
+      title: "License Plate Tolling",
+      company: "The Revenue Markets",
+      technologies: [".NET", "Angular", "Oracle", "REST APIs", "SOAP APIs", "NodeJS"],
+      description: "Developed a modern license plate-based tolling system, integrating UI for image review and billing, and interfacing with NYS DMV services.",
+      imageUrl: '',
+      githubLink: ''
+    },
+    {
+      title: "Azure DevOps Integration",
+      company: "The Revenue Markets",
+      technologies: [".NET", "TypeScript", "Azure"],
+      description: "Led migration from Team Foundation Server to Azure DevOps, implemented CI/CD pipelines, and improved security with Azure Key Vault.",
+      imageUrl: '',
+      githubLink: 'asd'
+    },
+    {
+      title: "Paesano's Pizzeria & Restaurant",
+      company: "MJPC Solutions",
+      technologies: ["Angular"],
+      description: "Developed a responsive static website for a local restaurant, focusing on SEO, accessibility, and hosting via Netlify.",
+      imageUrl: '',
+      githubLink: ''
+    },
+    {
+      title: "Vibration Test Bench",
+      company: "Connection Technology Center",
+      technologies: ["C#"],
+      description: "Designed and implemented a semi-automated vibration test system, including real-time monitoring, database integration, and report generation.",
+      imageUrl: '',
+      githubLink: ''
+    }
+  ];
 }
